577eea2f61949fb9a28554294c84aa76d9744179
hmetcalfe1 <h.metcalfe1@gmail.com>
2019-01-08 17:23:04 +0000

    very simple data import and produce ggplot

--- tidytues2019/tt_jan19/tt_08012019/tt08012019.R
+++ tidytues2019/tt_jan19/tt_08012019/tt08012019.R
@@ -1,5 +1,10 @@
 #tidytuesday 08012019
 library(tidyverse)
 
-importfile<-"IMDb_Economist_tv_ratings.csv"
+importfile<-"tidytues2019/tt_jan19/tt_08012019/IMDb_Economist_tv_ratings.csv"
 tvdata <- read_csv(importfile)
+tvdata
+
+ggplot(data = tvdata) +
+  geom_point(mapping = aes(x=date,y=av_rating))
+    
