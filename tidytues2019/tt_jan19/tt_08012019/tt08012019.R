53c14cba8fc53a6ae0499f8359d59b509e1d742a
hmetcalfe1 <h.metcalfe1@gmail.com>
2019-01-08 17:29:23 +0000

    save graphic to correct dir

--- tidytues2019/tt_jan19/tt_08012019/tt08012019.R
+++ tidytues2019/tt_jan19/tt_08012019/tt08012019.R
@@ -2,9 +2,12 @@
 library(tidyverse)
 
 importfile<-"tidytues2019/tt_jan19/tt_08012019/IMDb_Economist_tv_ratings.csv"
+exportfile<-"tidytues2019/tt_jan19/tt_08012019/08012019.pdf"
+  
 tvdata <- read_csv(importfile)
 tvdata
 
 ggplot(data = tvdata) +
   geom_point(mapping = aes(x=date,y=av_rating))
     
+ggsave(exportfile)
