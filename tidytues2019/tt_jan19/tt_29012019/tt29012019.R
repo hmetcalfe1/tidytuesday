858106535e6982b4ce2b23fa0a3ff7ce031cf807
hmetcalfe1 <h.metcalfe1@gmail.com>
2019-01-29 11:45:03 +0000

    finish cheese graph

--- tidytues2019/tt_jan19/tt_29012019/tt29012019.R
+++ tidytues2019/tt_jan19/tt_29012019/tt29012019.R
@@ -1,17 +1,28 @@
 #tidytuesday 29012019
+rm(list=ls())
 library(tidyverse)
 
 importfile<-"tidytues2019/tt_jan19/tt_29012019/clean_cheese.csv"
 exportfile<-"tidytues2019/tt_jan19/tt_08012019/29012019.pdf"
 
 chdata <- read_csv(importfile)
-chdata
 
-ggplot(data = chdata) +
-  geom_line(mapping = aes(x=Year,y=Cheddar), colour="deepskyblue3")+
-  geom_line(mapping = aes(x=Year,y=Mozzarella), colour="deeppink3")+
-  geom_line(mapping = aes(x=Year,y=`American Other`), colour="chocolate3")+
-  geom_line(mapping = aes(x=Year,y=`Italian other`), colour="chocolate3")+
-  scale_y_continuous()
+chdata<-mutate(chdata,
+       OtherCheese=rowSums(chdata[6:11],na.rm = TRUE)
+       )
+
+ggplot(data = chdata,aes(x=Year)) +
+  geom_line(mapping = aes(y=Cheddar), colour="deepskyblue3")+
+  geom_line(mapping = aes(y=Mozzarella), colour="deeppink3")+
+  geom_line(mapping = aes(y=`American Other`), colour="yellow3")+
+  geom_line(mapping = aes(y=`Italian other`), colour="forestgreen")+
+  geom_line(mapping = aes(y=OtherCheese), colour="chocolate3")+
+  ylab("Per capita consumption ofcheese / pounds")+
+  geom_text(aes(x = 2011, y = 11.5, label = "Mozzarella"), color = "deeppink3") + 
+  geom_text(aes(x = 2011, y = 9, label = "Cheddar"), color = "deepskyblue3")+
+  geom_text(aes(x = 2011, y = 4.5, label = "Other American Type Cheeses"), color = "yellow3") + 
+  geom_text(aes(x = 2011, y = 2, label = "Other Italian Type Cheeses"), color = "forestgreen")+
+  geom_text(aes(x = 2011, y = 6.5, label = "Other Cheeses"), color = "chocolate3")+
+  theme_bw()
 
 ggsave(exportfile)
